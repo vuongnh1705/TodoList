fe25ae7fe4c3e0714c23d66c54a8a5e6992d24c5
Nguyễn Hùng Vương <vuongnguyen.170599@gmail.com>
2019-10-20 23:26:15 +0700

    TodoList

---
+++ navigation/MainTabNavigator.js
@@ -0,0 +1,80 @@
+import React from 'react';
+import { Platform } from 'react-native';
+import { createStackNavigator, createBottomTabNavigator } from 'react-navigation';
+
+import TabBarIcon from '../components/TabBarIcon';
+import CompleteScreen from '../screens/CompleteScreen';
+import AllScreen from '../screens/AllScreen';
+import ActiveScreen from '../screens/ActiveScreen';
+import SingleTodoScreen from '../screens/SingleTodoScreen';
+
+const config = Platform.select({
+  web: { headerMode: 'screen' },
+  default: {},
+});
+
+const CompleteStack = createStackNavigator(
+  {
+    Complete: CompleteScreen,
+    SingleTodo: SingleTodoScreen
+  },
+  config
+);
+
+CompleteStack.navigationOptions = {
+  tabBarLabel: 'Complete',
+  tabBarIcon: ({ focused }) => (
+    <TabBarIcon
+      focused={focused}
+      name={Platform.OS === 'ios' ? 'ios-done-all' : 'md-done-all'}
+    />
+  ),
+};
+
+CompleteStack.path = '';
+
+const AllStack = createStackNavigator(
+  {
+    All: AllScreen,
+    SingleTodo: SingleTodoScreen,
+  },
+  config
+);
+
+AllStack.navigationOptions = {
+  tabBarLabel: 'All',
+  tabBarIcon: ({ focused }) => (
+    <TabBarIcon focused={focused}
+      name={Platform.OS === "ios"
+        ? `ios-add-circle${focused ? "-outline" : ""}`
+        : 'ios-add-circle-outline'} />
+  ),
+};
+
+AllStack.path = '';
+
+const ActiveStack = createStackNavigator(
+  {
+    Active: ActiveScreen,
+  },
+  config
+);
+
+ActiveStack.navigationOptions = {
+  tabBarLabel: 'Active',
+  tabBarIcon: ({ focused }) => (
+    <TabBarIcon focused={focused} name={Platform.OS === 'ios' ? 'ios-options' : 'md-options'} />
+  ),
+};
+
+ActiveStack.path = '';
+
+const tabNavigator = createBottomTabNavigator({
+  CompleteStack,
+  AllStack,
+  ActiveStack,
+});
+
+tabNavigator.path = '';
+
+export default tabNavigator;
